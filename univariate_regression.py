6e129de4c0b74b605509b87b5e7b78bfe8f1603f
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-06-05 03:44:53 +0430

    add error plot

--- univariate_regression.py
+++ univariate_regression.py
@@ -32,23 +32,13 @@ def step_gradient(given_b, given_m, given_x, given_y, given_learning_rate):
 
 
 def univariate_regression(given_x, given_y, given_b, given_m, given_learning_rate, given_num_of_iter):
-    x_array = np.array(given_x)[0]
-    y_array = np.array(given_y)[0]
-    plt.scatter(x_array, y_array, s=10, c='b', marker='o')
-    plt.axis([0, given_x.max(), 0, given_y.max()])
-    plt.ylabel('MEDV')
-    plt.xlabel('CRIM')
     current_b = given_b
     current_m = given_m
+    errors = [compute_error_for_line_given_points(given_b, given_m, given_x, given_y)]
     for i in range(given_num_of_iter):
-        # plt.plot(given_x, given_y, 'bo', given_x, current_m*given_x+current_b, 'r-')
-        # plt.scatter(x_array, current_m*x_array+current_b, s=10, c='r')
-        # plt.pause(0.005)
-        print(i)
         current_b, current_m = step_gradient(current_b, current_m, given_x, given_y, given_learning_rate)
-    plt.scatter(x_array, current_m * x_array + current_b, s=10, c='r')
-    plt.show()
-    return current_b, current_m
+        errors.append(compute_error_for_line_given_points(current_b, current_m, given_x, given_y))
+    return current_b, current_m, errors
 
 
 if __name__ == "__main__":
@@ -64,7 +54,23 @@ if __name__ == "__main__":
     # plt.ylabel('MEDV')
     # plt.xlabel('CRIM')
     # plt.show()
-    b, m = univariate_regression(x1, y, b, m, learning_rate, num_of_iter)
+    b, m, errors = univariate_regression(x1, y, b, m, learning_rate, num_of_iter)
     # b, m = univariate_regression(x2, y, b, m, learning_rate, num_of_iter)
+    plt.figure(1)
+    plt.axis([0, x1.max(), 0, y.max()])
+    plt.xlabel('CRIM')
+    plt.ylabel('MEDV')
+    x1_arr = np.array(x1)[0]
+    y_arr = np.array(y)[0]
+    s = m * x1 + b
+    s = np.array(s)[0]
+    plt.plot(x1_arr, y_arr, 'bo', x1_arr, s, 'r')
+
+    plt.figure(2)
+    plt.axis([0, 1000, 0, errors[0]])
+    plt.xlabel('iter')
+    plt.ylabel('err')
+    plt.plot(errors)
+    plt.show()
     err = compute_error_for_line_given_points(b, m, x1, y)
     print(err)
