d7503dca3dbc03d0f05eff244d9be3a95bdcbe25
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-06-13 21:46:05 +0430

    fix wrong implementation of l2 norm regularization

--- logistic_regression.py
+++ logistic_regression.py
@@ -15,27 +15,29 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-def compute_error(h, y, theta, landa=None):
-    l2norm = landa / 2 * np.sum(np.power(theta, 2)) if landa is not None else 0
-    return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()+ l2norm
+def compute_error(h, y, theta, landa):
+    l2norm = landa * np.sum(np.power(theta, 2))
+    return (-y * np.log(h) - (1 - y) * np.log(1 - h) + l2norm).mean()
 
 
-def logistic_regression(init_x, init_y, landa=None):
+def logistic_regression(init_x, init_y, learning_rate, num_of_iters, landa):
     theta = np.zeros((init_x.shape[1], 1))
-    for i in range(num_iter):
+    for i in range(num_of_iters):
         z = np.dot(init_x, theta)
         h = sigmoid(z)
-        l2norm = landa * theta if landa is not None else 0
-        gradient = np.dot(init_x.T, (h - init_y.T)) / init_y.size + l2norm
-        theta -= lr * gradient
+        l2norm = landa * theta
+        gradient = np.dot(init_x.T, (h - init_y.T)) + l2norm
+        theta -= learning_rate / init_y.size * gradient
         if i % 10000 == 0:
             print('compute_error: %f' % compute_error(h, init_y, theta, landa))
     return theta
 
+
 def predict(data_in, coeffs, threshold):
     return sigmoid(np.dot(data_in, coeffs)) >= threshold
 
-if __name__ == "__main__":
+
+def main():
     inputs = read_file()
     zeros_x = [x[0] for x in inputs if x[2] == 0]
     zeros_y = [x[1] for x in inputs if x[2] == 0]
@@ -52,11 +54,15 @@ if __name__ == "__main__":
               np.matrix(inputs)[:, 1].max() + 5])
     plt.plot(zeros_x, zeros_y, 'bo', ones_x, ones_y, 'r*')
     plt.show()
+    num_of_iters = 100000
+    learning_rate = 0.001
     landa = 0.01
-    lr = 0.001
-    num_iter = 100000
-    T = logistic_regression(X, Y, landa)
+    T = logistic_regression(X, Y, num_of_iters, learning_rate, landa)
     print(T)
     final_z = np.dot(X, T)
     final_h = sigmoid(final_z)
-    print('Final Error: %f'% compute_error(final_h, Y, T, landa))
+    print('Final Error: %f' % compute_error(final_h, Y, T, landa))
+
+
+if __name__ == "__main__":
+    main()
